7b3aea7cd3992df2fa43ff485dec0b296aed7590
ChoMinji <chominji98@2uin.com>
2022-12-16 15:39:26 +0900

    feat(props): 작성 중

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
-const Hello = () => {
+const Hello = (props) => {
 
   // let name = "minji";
   // 단순한 변수. 값이 바뀌어도 react가 인지하지 못함.
   // -> UI를 업데이트 해주지 않음
   const [name, setName] = useState("Minji");
+  const [age, setAge] = useState(props.age);
 
   function changeName() {
     setName(name === "Minji" ? "MinYoung" : "Minji");
@@ -14,7 +15,9 @@ const Hello = () => {
   return (
     <div>
       <h1>state</h1>
-      <h2>{name}</h2>
+      <h2 id="name">
+        {name}({props.age})
+      </h2>
       <button onClick={changeName}>Change</button>
     </div>
   );
